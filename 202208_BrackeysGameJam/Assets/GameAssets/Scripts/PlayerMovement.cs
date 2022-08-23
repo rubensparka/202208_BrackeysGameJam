1b3c021909dfe39ebe4ba80fd250adfc9f71347a
Goschoki <thegameboy_90@web.de>
2022-08-23 18:22:52 +0200

    Animations

--- 202208_BrackeysGameJam/Assets/GameAssets/Scripts/PlayerMovement.cs
+++ 202208_BrackeysGameJam/Assets/GameAssets/Scripts/PlayerMovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Transform m_WallCheck;
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
     public float direction = 1f;
@@ -27,17 +29,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("Jump", true);
         }
 
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
+            animator.SetBool("Crouch", true);
+
             m_WallCheck.position = new Vector2(m_WallCheck.position.x, m_WallCheck.position.y - 0.4f);
 
 
         } else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
+            animator.SetBool("Crouch", false);
+
             m_WallCheck.position = new Vector2(m_WallCheck.position.x, m_WallCheck.position.y + 0.4f);
         }
     }
@@ -49,12 +56,17 @@ public class PlayerMovement : MonoBehaviour
         jump = false;
     }
 
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.name == "Spikes")
         {
-            Destroy(GameObject.Find("Player"));
-            Debug.Log("aua");
+            //Destroy(GameObject.Find("Player"));
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
